9ad7bdb13767a802efb6094029d7b3e3583de0c2
fGHG99 <azkalearn10@gmail.com>
2025-08-13 11:32:34 +0700

    Day 3, Pengunaan Do while & Refactor Function

--- Day_3/utils/fileHandler.js
+++ Day_3/utils/fileHandler.js
@@ -2,19 +2,16 @@ const fs = require('fs')
 
 const fileHandler = {
     readFile: (loc) => {
-        if (!loc) {
-            console.error("the file is null")
-        }
-
         if (!fs.existsSync(loc)) {
             fs.writeFileSync(loc, "[]");
+            console.log("File not found, created a new file at ", loc);
         }
 
         return fs.readFileSync(loc, "utf-8");
     },
     writeFile: (loc, data) => {
         if (!loc) {
-            console.error("the file is null");
+            console.log("the file is null");
             return;
         }
         fs.writeFileSync(loc, JSON.stringify(data, null, 2));
@@ -28,4 +25,4 @@ const savingData = (filePath, data) => {
     fileHandler.writeFile(filePath, jsonData);
 }
 
-module.exports = { savingData };
+module.exports = { savingData, fileHandler };
